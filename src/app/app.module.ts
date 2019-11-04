b7490b56253c7e8c2bf30065b15dd980a282a23a
duredem <doured.mzakzak@gmail.com>
2019-11-04 23:04:49 +0100

    sistemazione loginpage con animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
 import { GameDashboardModule } from './game-dashboard/game-dashboard.module';
 import {HttpClientModule} from '@angular/common/http';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     LoginModule,
-    GameDashboardModule
+    GameDashboardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

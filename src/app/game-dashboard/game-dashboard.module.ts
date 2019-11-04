42602efb39526552cb4c8cc5ad240ca9ea5e5da6
michaelcucchietti94 <michael.cucchietti@gmail.com>
2019-11-04 17:46:59 +0100

    creato map component (definizione)

--- src/app/game-dashboard/game-dashboard.module.ts
+++ src/app/game-dashboard/game-dashboard.module.ts
@@ -5,11 +5,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { RankingComponent } from './ranking/ranking.component';
 import { DashboardHeaderComponent } from './dashboard-header/dashboard-header.component';
 import { ProfileComponent } from './profile/profile.component';
+import { MapComponent } from './map/map.component';
 
 
 
 @NgModule({
-  declarations: [DashboardComponent, DashboardHeaderComponent, ProfileComponent, RankingComponent],
+  declarations: [DashboardComponent, DashboardHeaderComponent, ProfileComponent, RankingComponent, MapComponent],
   imports: [
     CommonModule,
     GameRoutingModule

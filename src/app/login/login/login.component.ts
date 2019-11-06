e8c16b87fccee6ee7b30944f4e007baf0997f086
duredem <doured.mzakzak@gmail.com>
2019-11-06 16:25:46 +0100

    aggiornamenti fronend

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -16,22 +16,43 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
     /*this.login();*/
     /*this.login(null);*/
+    this.logoFadeOut();
   }
   
+  logoFadeOut() {
+    setTimeout(() => {
+      let logoElement = document.getElementById('contrainingLogo');
+      logoElement.classList.add('fadeOutAnimation');
+    }, 1500);
+    setTimeout(() => {
+      let logoElement = document.getElementById('contrainingLogo');
+      logoElement.classList.add('displayNone');
+    }, 4500);
+  }
+  
+  matrixEffect(){
+    let matrixElement = document.getElementById('matrix');
+    let cerchioElement = document.getElementById('cerchio');
+    matrixElement.classList.add('fadeInAnimation');
+    cerchioElement.classList.add('speedUp');
+  }
 
   login(f: NgForm) {
-    let credentials : LoginCredentials = new LoginCredentials(f.value.username, f.value.password);
-    //let credentials : LoginCredentials = new LoginCredentials('user1', 'user');
-    this.service.login(credentials).subscribe((user) => {
-      if(user == null)
-        return;
+    setTimeout(() =>{
+      let credentials : LoginCredentials = new LoginCredentials(f.value.username, f.value.password);
+      //let credentials : LoginCredentials = new LoginCredentials('user1', 'user');
+      this.service.login(credentials).subscribe((user) => {
+        if(user == null)
+          return;
 
-      localStorage.setItem('currentUser', JSON.stringify(user));
-      if(user.usertype.toString().toUpperCase() === 'ADMIN')
-        alert('no admin for now');
-      else 
-        this.router.navigate(['/dashboard']);
-    });
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        if(user.usertype.toString().toUpperCase() === 'ADMIN')
+          alert('no admin for now');
+        else 
+          this.router.navigate(['/dashboard']);
+      });
+    }, 1500);
+    
 
   }
 

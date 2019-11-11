2fc6246e99f961d8292ed9da498272983eff7c2b
Maria <mariellariccardi06@gmail.com>
2019-11-11 12:49:27 +0100

    register.ts

--- src/service/user/User.service.ts
+++ src/service/user/User.service.ts
@@ -4,6 +4,7 @@ import { User } from 'src/dto/User';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Ranking } from 'src/dto/game/Ranking';
+import { Usertype } from 'src/dto/Usertype';
 
 @Injectable({
     providedIn:'root'
@@ -17,14 +18,20 @@ export class UserService extends AbstractService<User> {
     dailyRanking() : Observable<Ranking[]> {
         return this.http.get<Ranking[]>(this.getService() + '/dailyranking');
     }
-    
+
     weeklyRanking() : Observable<Ranking[]> {
         return this.http.get<Ranking[]>(this.getService() + '/weeklyranking');
     }
-    
+
     levelRanking(livello : number) : Observable<Ranking[]> {
         return this.http.get<Ranking[]>(this.getService() + '/levelranking_' + livello);
     }
+
+    insert(dto: User): Observable<any> {
+        dto.livello=1;
+        dto.usertype=Usertype.USER;
+       return this.http.post('http://localhost:' + this.port + '/user/insert', dto);
+     }
 
 
 }

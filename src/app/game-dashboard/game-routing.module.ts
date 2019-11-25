2f6de8d0342b8181ff6f4998f0b5fe2e0695df2b
michaelcucchietti94 <michael.cucchietti@gmail.com>
2019-11-25 22:48:26 +0100

    standardizzato box

--- src/app/game-dashboard/game-routing.module.ts
+++ src/app/game-dashboard/game-routing.module.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
 
 const routes : Routes = [
-    {path: 'dashboard', component: DashboardComponent}
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'tutorial', component: TutorialComponent}
 ]
 @NgModule({
     imports: [RouterModule.forRoot(routes)],

6b336f939a24db41774f24620fd7637544cacc24
michaelcucchietti94 <michael.cucchietti@gmail.com>
2019-11-28 12:31:05 +0100

    modifica logo + conversione AnimaFramework in TS

--- src/app/game-dashboard/user-profile/user-profile/user-profile.component.ts
+++ src/app/game-dashboard/user-profile/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilityService } from 'src/service/utility/Utility.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private utility : UtilityService) { }
 
   ngOnInit() {
+    
+  }
+
+  inputMouseHover(elem : HTMLElement) : void {
+    this.utility.animationFactory.AnimationLeft(elem, 1, 'linear', '30vh').start();
   }
 
 }
